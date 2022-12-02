b779ade289808a9ca99013484b3c09a3d3645e91
AbhishekNeela1995 <90269102+AbhishekNeela1995@users.noreply.github.com>
2022-12-02 11:45:09 +0530

    Update BaseTest.java

--- src/test/java/com/tests/BaseTest.java
+++ src/test/java/com/tests/BaseTest.java
@@ -13,9 +13,10 @@ import java.net.URL;
 public class BaseTest {
 
     protected WebDriver driver;
+    protected static ThreadLocal<WebDriver> tdriver = new ThreadLocal<WebDriver>();
 
     @BeforeTest
-    public void setupDriver(ITestContext ctx) throws MalformedURLException {
+    public setupDriver(ITestContext ctx) throws MalformedURLException {
         // BROWSER => chrome / firefox
         // HUB_HOST => localhost / 10.0.1.3 / hostname
 
@@ -38,6 +39,12 @@ public class BaseTest {
         String completeUrl = "http://" + host + ":4444/wd/hub";
         dc.setCapability("name", testName);
         this.driver = new RemoteWebDriver(new URL(completeUrl), dc);
+        tdriver.set(this.driver);
+        return getDriver();
+    }
+    
+    public static synchronized WebDriver getDriver() {
+    return tdriver.get();
     }
 
     @AfterTest
