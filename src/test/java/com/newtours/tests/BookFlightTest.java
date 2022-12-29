b7aff3965c03048c441c48dd1143615484dc1971
Nareshnani212 <nareshmulukutla@gmail.com>
2022-12-29 14:39:35 +0530

    Allure Implemented

--- src/test/java/com/newtours/tests/BookFlightTest.java
+++ src/test/java/com/newtours/tests/BookFlightTest.java
@@ -2,11 +2,21 @@ package com.newtours.tests;
 
 import com.newtours.pages.*;
 import com.tests.BaseTest;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+@Epic("Book Flight Functionality")
+@Feature("Verify Book Flight Functionality")
 public class BookFlightTest extends BaseTest {
 
     private String noOfPassengers;
@@ -20,6 +30,9 @@ public class BookFlightTest extends BaseTest {
     }
 
     @Test
+    @Description("Test Case #1, verify Registration Feature")
+	@Severity(SeverityLevel.BLOCKER)
+	@Story("Test Case #1, verify Registration Feature")
     public void registrationPage() {
         RegistrationPage registrationPage = new RegistrationPage(driver);
         registrationPage.goTo();
@@ -29,12 +42,18 @@ public class BookFlightTest extends BaseTest {
     }
 
     @Test(dependsOnMethods = "registrationPage")
+    @Description("Test Case #2, verify Registration Confirmation Feature")
+	@Severity(SeverityLevel.BLOCKER)
+	@Story("Test Case #2, verify Registration Confirmation Feature")
     public void registrationConfirmationPage(){
         RegistrationConfirmationPage registrationConfirmationPage = new RegistrationConfirmationPage(driver);
         registrationConfirmationPage.goToFlightDetailsPage();
     }
 
     @Test(dependsOnMethods = "registrationConfirmationPage")
+    @Description("Test Case #3, verify Flight Details Feature")
+	@Severity(SeverityLevel.BLOCKER)
+	@Story("Test Case #3, verify Flight Details Feature")
     public void flightDetailsPage(){
         FlightDetailsPage flightDetailsPage = new FlightDetailsPage(driver);
         flightDetailsPage.selectPassengers(noOfPassengers);
@@ -42,6 +61,9 @@ public class BookFlightTest extends BaseTest {
     }
 
     @Test(dependsOnMethods = "flightDetailsPage")
+    @Description("Test Case #4, verify Find Flight Feature")
+	@Severity(SeverityLevel.BLOCKER)
+	@Story("Test Case #4, verify Find Flight Feature")
     public void findFlightPage(){
         FindFlightPage findFlightPage = new FindFlightPage(driver);
         findFlightPage.submitFindFlightPage();
@@ -49,6 +71,9 @@ public class BookFlightTest extends BaseTest {
     }
 
     @Test(dependsOnMethods = "findFlightPage")
+    @Description("Test Case #5, verify Flight Confirmation Feature")
+	@Severity(SeverityLevel.BLOCKER)
+	@Story("Test Case #5, verify Flight Confirmation Feature")
     public void flightConfirmationPage(){
         FlightConfirmationPage flightConfirmationPage = new FlightConfirmationPage(driver);
         String actualPrice = flightConfirmationPage.getPrice();
--- src/test/java/com/searchmodule/tests/SearchTest.java
+++ src/test/java/com/searchmodule/tests/SearchTest.java
@@ -2,13 +2,26 @@ package com.searchmodule.tests;
 
 import com.searchmodule.pages.SearchPage;
 import com.tests.BaseTest;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+@Epic("Search Functionality")
+@Feature("Verify Search Functionality")
 public class SearchTest extends BaseTest {
 
     @Test
+    @Description("Test Case #1, verify Search Feature")
+	@Severity(SeverityLevel.BLOCKER)
+	@Story("Test Case #1, verify Search Feature")
     @Parameters({"keyword"})
     public void search(String keyword){
         SearchPage searchPage = new SearchPage(driver);
